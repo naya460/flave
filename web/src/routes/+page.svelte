99ef88ba8044b44eba6c100151ab8825cd909951
naya460 <76655448+naya460@users.noreply.github.com>
2024-02-11 18:23:57 +0900

    Run lint and format when commit

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script lang="ts">
+	let n = 0;
+	function a(b: number, c: number) {
+		return b + c;
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>{n}</p>
+<button
+	on:click={() => {
+		n = a(n, 1);
+	}}>button</button
+>
