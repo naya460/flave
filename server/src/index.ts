10a4a7a4099c02353bba468448fd0be0c603ca56
naya460 <76655448+naya460@users.noreply.github.com>
2024-02-12 18:26:47 +0900

    Run lint and format when commit in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,11 @@
-import Fastify from "fastify";
-import cors from "@fastify/cors";
 import fastify from "fastify";
+import cors from "@fastify/cors";
 
-const server = Fastify();
+const server = fastify();
 server.register(cors, { origin: true });
 
-server.get("/test", async (req, res) => {
-    return "This is a server!";
+server.get("/test", async () => {
+  return "This is a server!";
 });
 
-server.listen({"host": "0.0.0.0", "port": 8080});
+server.listen({ host: "0.0.0.0", port: 8080 });
