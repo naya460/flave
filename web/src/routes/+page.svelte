603abebfcb9f31bd225678681ede1d6ee40abacb
naya460 <76655448+naya460@users.noreply.github.com>
2024-02-12 22:12:02 +0900

    Lint and format the files without husky

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -3,9 +3,4 @@
   export let data: PageData;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
-
-<p>{data.text}</p>
+<p>server response : {data.text}</p>
