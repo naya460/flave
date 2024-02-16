4eb860e2aa2eb0eed2d459fb9b86806bc54fc11a
naya460 <76655448+naya460@users.noreply.github.com>
2024-02-16 23:36:20 +0900

    Prepare the mongodb server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,15 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient("mongodb://root:password@mongo:27017/");
+client.connect().then(console.log).catch(console.error);
 
 const server = fastify();
 server.register(cors, { origin: true });
 
 server.get("/test", async () => {
-  return "This is a server!";
+  return await client.db("test").collection("fuwah").find({}).toArray();
 });
 
 server.listen({ host: "0.0.0.0", port: 8080 });
