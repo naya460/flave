1f510bc6922d99e5cbca0573c5ea7cd426584257
naya460 <76655448+naya460@users.noreply.github.com>
2024-03-05 21:10:50 +0900

    Indicate the user list on admin manager page

---
+++ web/src/routes/manager/+page.svelte
@@ -0,0 +1,5 @@
+<a href="/">top page</a>
+
+<ul>
+  <li><a href="/manager/admin">admin</a></li>
+</ul>
---
+++ web/src/routes/manager/admin/+layout.svelte
@@ -0,0 +1,14 @@
+<div class="top">
+  <nav>
+    <a href="/manager">^ manager</a>
+    <div>ADMIN</div>
+  </nav>
+  <div><slot /></div>
+</div>
+
+<style>
+  .top {
+    display: grid;
+    grid-template-columns: 10rem 1fr;
+  }
+</style>
