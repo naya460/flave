f69929dddfdb1fa279e858d8efcd0154bdafcdce
naya460 <76655448+naya460@users.noreply.github.com>
2024-03-12 18:14:38 +0900

    Apply the expire time for sessions

--- server/src/mongo/collections/flave/session.ts
+++ server/src/mongo/collections/flave/session.ts
@@ -3,8 +3,11 @@ import { ObjectId } from "mongodb";
 
 type flvSessionType = {
   user_id: ObjectId;
+  created_at: Date;
 };
 
 export const flvSessionCollection = mongoClient
   .db("flave")
   .collection<flvSessionType>("session");
+
+flvSessionCollection.createIndex({ created_at: 1 }, { expireAfterSeconds: 10 });
--- server/src/mongo/session/create_session.ts
+++ server/src/mongo/session/create_session.ts
@@ -2,7 +2,10 @@ import { flvSessionCollection } from "mongo/collections/flave/session";
 import { ObjectId } from "mongodb";
 
 export const createSession = async (user_id: ObjectId) => {
-  const result = await flvSessionCollection.insertOne({ user_id });
+  const result = await flvSessionCollection.insertOne({
+    user_id,
+    created_at: new Date(),
+  });
 
   if (result.acknowledged) {
     return result.insertedId;
