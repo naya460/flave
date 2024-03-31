b95b2156d09aacafdbf67cb028cc2457d1c05646
naya460 <76655448+naya460@users.noreply.github.com>
2024-03-31 22:35:32 +0900

    Split files about create user in user management page for admin

--- web/src/routes/manager/admin/users/+page.svelte
+++ web/src/routes/manager/admin/users/+page.svelte
@@ -1,30 +1,9 @@
 <script lang="ts">
   import type { PageData } from "./$types";
   import Cards from "$lib/gui/common/Cards.svelte";
-  import Button from "$lib/gui/common/Button.svelte";
-  import Popup from "$lib/gui/common/Popup.svelte";
-  import TextInput from "$lib/gui/common/TextInput.svelte";
+  import CreateUser from "./CreateUser.svelte";
 
   export let data: PageData;
-
-  let hidden_popup = true;
-
-  const onSubmit = async (
-    event: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement }
-  ) => {
-    const form_data = new FormData(event.currentTarget);
-    const data = JSON.stringify({
-      auth_id: form_data.get("auth_id"),
-      password: form_data.get("password"),
-      confirm_password: form_data.get("confirm_password"),
-    });
-    await fetch(`http://${location.hostname}:8080/users`, {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: data,
-    });
-    location.reload();
-  };
 
   async function getUserWorkspaceList(user_id: string) {
     const res = await fetch(
@@ -36,9 +15,7 @@
   }
 </script>
 
-<Button style="text" on:click={() => (hidden_popup = false)}>
-  Create User
-</Button>
+<CreateUser />
 
 <Cards items={data.items}>
   <div slot="item" let:item>
@@ -59,35 +36,3 @@
     {/await}
   </div>
 </Cards>
-
-<Popup bind:hidden={hidden_popup}>
-  <form on:submit={onSubmit} class="form">
-    <label for="auth_id">auth_id</label>
-    <TextInput type="text" id="auth_id" name="auth_id" autocomplete="off" />
-    <label for="password">password</label>
-    <TextInput
-      type="password"
-      id="password"
-      name="password"
-      autocomplete="off"
-    />
-    <label for="confirm_password">confirm_password</label>
-    <TextInput
-      type="password"
-      id="confirm_password"
-      name="confirm_password"
-      autocomplete="off"
-    />
-    <Button style="filled">add</Button>
-  </form>
-</Popup>
-
-<style>
-  .form {
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: auto;
-    width: 100%;
-    gap: 0.2rem;
-  }
-</style>
---
+++ web/src/routes/manager/admin/users/CreateUser.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import Button from "$lib/gui/common/Button.svelte";
+  import Popup from "$lib/gui/common/Popup.svelte";
+  import TextInput from "$lib/gui/common/TextInput.svelte";
+
+  let hidden_popup = true;
+
+  const onSubmit = async (
+    event: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement }
+  ) => {
+    const form_data = new FormData(event.currentTarget);
+    const data = JSON.stringify({
+      auth_id: form_data.get("auth_id"),
+      password: form_data.get("password"),
+      confirm_password: form_data.get("confirm_password"),
+    });
+    await fetch(`http://${location.hostname}:8080/users`, {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: data,
+    });
+    location.reload();
+  };
+</script>
+
+<Button style="text" on:click={() => (hidden_popup = false)}>
+  Create User
+</Button>
+
+<Popup bind:hidden={hidden_popup}>
+  <form on:submit={onSubmit} class="form">
+    <label for="auth_id">auth_id</label>
+    <TextInput type="text" id="auth_id" name="auth_id" autocomplete="off" />
+    <label for="password">password</label>
+    <TextInput
+      type="password"
+      id="password"
+      name="password"
+      autocomplete="off"
+    />
+    <label for="confirm_password">confirm_password</label>
+    <TextInput
+      type="password"
+      id="confirm_password"
+      name="confirm_password"
+      autocomplete="off"
+    />
+    <Button style="filled">add</Button>
+  </form>
+</Popup>
+
+<style>
+  .form {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto;
+    width: 100%;
+    gap: 0.2rem;
+  }
+</style>
