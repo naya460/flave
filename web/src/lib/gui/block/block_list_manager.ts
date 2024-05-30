8dcb819e728333535904df196393310782a8a43f
naya460 <76655448+naya460@users.noreply.github.com>
2024-05-30 21:38:08 +0900

    Ignore rdb view when moving caret

--- web/src/lib/gui/block/block_list_manager.ts
+++ web/src/lib/gui/block/block_list_manager.ts
@@ -7,15 +7,21 @@ export function setBlockList(list: blockListStore) {
 	block_list = list;
 }
 
+function getTextBlockList() {
+	return get(block_list).filter(w => {
+		return w.type === "paragraph" || w.type === "heading";
+	});
+}
+
 export function moveCaretUp(current_block_id: string) {
 	const selection = window.getSelection();
 	if (selection === null) return;
 
-	const pos = get(block_list).findIndex((v) => v._id === current_block_id);
+	const pos = getTextBlockList().findIndex((v) => v._id === current_block_id);
 	if (pos < 1) return;
 
 	if (selection.isCollapsed) {
-		const node = get(block_list)[pos - 1].dom_node?.childNodes[0];
+		const node = getTextBlockList()[pos - 1].dom_node?.childNodes[0];
 		if (node === undefined || node.nodeValue === null) return;
 		const offset =
 			selection.anchorOffset > node.nodeValue.length
@@ -29,11 +35,11 @@ export function moveCaretDown(current_block_id: string) {
 	const selection = window.getSelection();
 	if (selection === null) return;
 
-	const pos = get(block_list).findIndex((v) => v._id === current_block_id);
-	if (pos < 0 || pos > get(block_list).length - 1) return;
+	const pos = getTextBlockList().findIndex((v) => v._id === current_block_id);
+	if (pos < 0 || pos > getTextBlockList().length - 2) return;
 
 	if (selection.isCollapsed) {
-		const node = get(block_list)[pos + 1].dom_node?.childNodes[0];
+		const node = getTextBlockList()[pos + 1].dom_node?.childNodes[0];
 		if (node === undefined || node.nodeValue === null) return;
 		const offset =
 			selection.anchorOffset > node.nodeValue.length
