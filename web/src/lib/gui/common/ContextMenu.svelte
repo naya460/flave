9c9a391e5ea99c932fc82066c1ce58fc3072596c
naya460 <76655448+naya460@users.noreply.github.com>
2024-07-10 12:39:28 +0900

    Reimplement the context menu component with popover

--- web/src/lib/gui/common/ContextMenu.svelte
+++ web/src/lib/gui/common/ContextMenu.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { afterUpdate } from "svelte";
   import Button from "./Button.svelte";
 
   type Item =
@@ -29,66 +30,86 @@
   };
 
   let selecting_submenu: null | number = null;
+
+  let contents_node: HTMLDivElement;
+  let top_node: HTMLDivElement;
+
+  let content_position_x: number;
+  let content_position_y: number;
+
+  afterUpdate(() => {
+    if (hidden === true) {
+      contents_node.hidePopover();
+    } else {
+      content_position_x = top_node.getBoundingClientRect().x;
+      content_position_y = top_node.getBoundingClientRect().y;
+      contents_node.showPopover();
+    }
+  });
 </script>
 
-{#if hidden === false}
-  <div class="top" style:position={"relative"}>
-    {#if submenu === false}
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
-      <!-- svelte-ignore a11y-no-static-element-interactions -->
-      <div on:click={setHidden} class="background" />
+<div class="top" style:position={"relative"} bind:this={top_node}>
+  {#if submenu === false && hidden === false}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div on:click={setHidden} class="background" />
+  {/if}
+  <!-- <div on:click={setHidden} class="background" /> -->
+  <div
+    class="contents"
+    popover="manual"
+    bind:this={contents_node}
+    style:left={`${content_position_x}px`}
+    style:top={`${content_position_y}px`}
+  >
+    {#if $$slots.default === true}
+      <slot />
+    {:else}
+      {#each contents as item, i}
+        {#if "contents" in item}
+          <Button
+            on:click={() => (selecting_submenu = i)}
+            style={{
+              buttonStyle: "text",
+              width: "100%",
+              height: "2rem",
+              textAlign: "left",
+            }}
+          >
+            {item.name}
+            {">"}
+          </Button>
+          <div style:position="absolute">
+            <svelte:self
+              contents={item.contents}
+              hidden={selecting_submenu !== i}
+              submenu={true}
+              hide_parent={setHidden}
+            />
+          </div>
+        {:else if "onClick" in item}
+          <Button
+            on:click={() => {
+              item.onClick();
+              setHidden();
+            }}
+            on:mouseenter={() => (selecting_submenu = null)}
+            style={{
+              buttonStyle: "text",
+              width: "100%",
+              height: "2rem",
+              textAlign: "left",
+            }}
+          >
+            {item.name}
+          </Button>
+        {:else}
+          <div>ERROR</div>
+        {/if}
+      {/each}
     {/if}
-    <!-- <div on:click={setHidden} class="background" /> -->
-    <div class="contents">
-      {#if $$slots.default === true}
-        <slot />
-      {:else}
-        {#each contents as item, i}
-          {#if "contents" in item}
-            <Button
-              on:click={() => (selecting_submenu = i)}
-              style={{
-                buttonStyle: "text",
-                width: "100%",
-                height: "2rem",
-                textAlign: "left",
-              }}
-            >
-              {item.name}
-              {">"}
-            </Button>
-            <div style:position="absolute">
-              <svelte:self
-                contents={item.contents}
-                hidden={selecting_submenu !== i}
-                submenu={true}
-                hide_parent={setHidden}
-              />
-            </div>
-          {:else if "onClick" in item}
-            <Button
-              on:click={() => {
-                item.onClick();
-                setHidden();
-              }}
-              on:mouseenter={() => (selecting_submenu = null)}
-              style={{
-                buttonStyle: "text",
-                width: "100%",
-                height: "2rem",
-                textAlign: "left",
-              }}
-            >
-              {item.name}
-            </Button>
-          {:else}
-            <div>ERROR</div>
-          {/if}
-        {/each}
-      {/if}
-    </div>
   </div>
-{/if}
+</div>
 
 <style>
   .top {
@@ -112,9 +133,7 @@
   }
 
   .contents {
-    position: absolute;
-    left: 0;
-    top: 0;
+    position: fixed;
     background-color: rgb(255, 255, 255);
     width: 20rem;
     height: 20rem;
@@ -122,5 +141,7 @@
     box-sizing: border-box;
     border-radius: 0.5rem;
     box-shadow: 0 0 6px -2px rgba(0, 0, 0, 128);
+    border: none;
+    margin: 0;
   }
 </style>
